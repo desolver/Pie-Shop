9bed7de899af746c8c967685fcbb4c72b321dee5
VladSavitskiy <cyberl1fe@yandex.ru>
2021-06-16 18:47:12 +0500

    Created basic views and settings for it

---
+++ PieShop/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using PieShop.Models;
+
+namespace PieShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
